cc0318be4edffcafee59868aa586f43d14a4f6d6
Joe Morris <joe.morris.dev@gmail.com>
2016-12-26 22:10:37 -0600

    added additional fields

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -14,11 +14,36 @@ public class Patient {
     int id;
 
     @Column (nullable = false)
-    String firstName;
+    String first_name;
 
     @Column (nullable = true)
-    String middleName;
+    String middle_name;
 
     @Column (nullable = false)
-    String lastName;
+    String last_name;
+
+    //TODO: replace string with some date thing
+    @Column (nullable = false)
+    String date_of_birth;
+
+    @Column(nullable = true)
+    String date_of_death;
+
+    //TODO: think about security for this. Make private?
+    @Column(nullable = false)
+    int SSN;
+
+    @Column(nullable = false)
+    String gender;
+
+    //TODO: figure out how to do address properly
+    @Column(nullable = false)
+    String address;
+
+    @Column(nullable = false)
+    String phone_number;
+
+    //pcp
+
+    //date created
 }
